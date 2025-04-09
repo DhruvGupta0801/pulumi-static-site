e8a6d4994667ff2e41988a410fa157bea794a6d6
DhruvGupta0801 <85752740+DhruvGupta0801@users.noreply.github.com>
2025-04-09 16:55:47 -0400

    Use BucketPublicAccessBlock to disable BlockPublicPolicy, include dev2 and dev3 config

--- index.ts
+++ index.ts
@@ -6,9 +6,15 @@ const bucket = new aws.s3.Bucket("my-static-site-bucket", {
     website: {
         indexDocument: "index.html",
     },
-    // Disable BlockPublicPolicy to allow the public bucket policy
+});
+
+// Configure the S3 bucket's public access settings to allow public bucket policies
+const bucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock("bucketPublicAccessBlock", {
+    bucket: bucket.bucket,
     blockPublicAcls: false,
     blockPublicPolicy: false,
+    ignorePublicAcls: false,
+    restrictPublicBuckets: false,
 });
 
 // Upload a sample index.html file to the S3 bucket
@@ -32,43 +38,3 @@ const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
                 Resource: `${arn}/*`,
             },
         ],
-    })),
-});
-
-// Create a CloudFront distribution to serve the S3 bucket content
-const distribution = new aws.cloudfront.Distribution("my-distribution", {
-    enabled: true,
-    origins: [
-        {
-            domainName: bucket.bucketRegionalDomainName,
-            originId: bucket.arn,
-        },
-    ],
-    defaultRootObject: "index.html",
-    defaultCacheBehavior: {
-        targetOriginId: bucket.arn,
-        viewerProtocolPolicy: "redirect-to-https",
-        allowedMethods: ["GET", "HEAD", "OPTIONS"],
-        cachedMethods: ["GET", "HEAD", "OPTIONS"],
-        forwardedValues: {
-            queryString: false,
-            cookies: { forward: "none" },
-        },
-        minTtl: 0,
-        defaultTtl: 86400,
-        maxTtl: 31536000,
-    },
-    priceClass: "PriceClass_100",
-    viewerCertificate: {
-        cloudfrontDefaultCertificate: true,
-    },
-    restrictions: {
-        geoRestriction: {
-            restrictionType: "none",
-        },
-    },
-});
-
-// Export the S3 bucket name and CloudFront distribution URL
-export const bucketName = bucket.bucket;
-export const websiteUrl = distribution.domainName;
